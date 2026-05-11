11ed34205162a51693e1d49628121853852989e4
Leonardo Kennedy <leokbrl1342008@gmail.com>
2026-05-11 11:22:43 -0300

    primeiro commit

---
+++ kderestart.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo '
+
+
+Realizando: killall plasmashell; kstart plasmashell
+
+
+'
+
+killall plasmashell;kstart plasmashell;
---
+++ update-all.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo '
+
+
+realizando: sudo nala update && sudo nala upgrade && flatpak upgrade
+
+
+'
+
+sudo nala update && sudo nala upgrade && flatpak upgrade
