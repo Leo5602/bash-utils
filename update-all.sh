35ec1fec153a052056e553c2df0714ac57f80b3c
Leonardo Kennedy <leokbrl1342008@gmail.com>
2026-05-18 10:14:04 -0300

    compactando código

--- update-all.sh
+++ update-all.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
 echo -e "\n\n\nrealizando: sudo nala update && sudo nala upgrade\n\n\n"
-
 sudo nala update && sudo nala upgrade
 
 echo -e "\n\n\nrealizando: flatpak upgrade\n\n\n"
-
-
 flatpak upgrade
