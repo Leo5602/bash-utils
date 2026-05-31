e5822fe0084a3920edaaf76befd2afb79560d65a
Leonardo Kennedy <leokbrl1342008@gmail.com>
2026-05-31 17:14:15 -0300

    corrigindo erros...

--- kderestart.sh
+++ kderestart.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-kill='killall plasmashell';
-start='kstart plasmashell';
+kill='killall plasmashell'
+start='kstart plasmashell'
 
-echo 'deseja mesmo fazer isso?(S/s/N/n)';
-read deseja;
+echo 'deseja mesmo fazer isso?(S/s/N/n)'
+read deseja
 
-if [ $deseja='S' ] || [$deseja='s']; then
-    echo -e "\n\ndesligando...\n";$($kill);
-    echo -e "\ndesligado";
-    echo -e "\nligando...\n";$($start);
+if [[ $deseja == 'S' || $deseja == 's' ]]; then
+    echo -e "\n\ndesligando...\n"
+    $kill
+    echo -e "\nligando...\n"
+    $start
     echo -e "\nligado"
-elif [ $deseja = 'N'] || [$deseja = 'n']; then
-    echo -e 'cancelando...';
+elif [[ $deseja == 'N' || $deseja == 'n' ]]; then
+    echo -e 'cancelando...'
 else
-    echo 'opção inválida';
+    echo 'opção inválida'
 fi
